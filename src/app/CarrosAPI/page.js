fbe2238769d8d20782cffe0d092a7b9d0b081e5b
glayberson <glaybersonsilva@outlook.com>
2023-06-16 16:50:52 -0300

    api quase feita

--- src/app/CarrosAPI/page.js
+++ src/app/CarrosAPI/page.js
@@ -1,26 +1,34 @@
 import styles from "../styles/Carros.module.css"
-export default async function CarrosAPI(){
+import Image from "next/image"
 
-const response = await fetch("https://apitrainees.herokuapp.com/carros")
-const data = await response.json()
-
-const car = data
-    return (
-       <>
-
-       <h1>Carros:</h1>
-       <ul className={styles.listcar}>
-        {data.map((data) => (
-            <li key={data.id}>{data.nome}</li>
-            
-        ))}
-
-        {data.map((data) => (
-            <li key={data.id}>{data.img}</li>
-        ))}
-        </ul>
-       </>
-        
-    )
-    
+const getCarsData = async () => {
+    const response = await fetch("https://apitrainees.herokuapp.com/carros")
+    return response.json()
 }
+
+export default async function CarrosAPI(){
+    const cars = await getCarsData()
+    return (
+        <div className={styles.listcar}> 
+           
+        <div>
+    
+        {cars.map((car) => {
+                return console.log(car.img), <img src={cars.img} alt="Carros" width={300} height={300}></img>
+            })}
+        
+        </div>
+
+
+        <div>
+
+            {cars.map((car) => {
+                return <p>{car.nome}: {car.descricao}: {car.preco}</p>
+            })}
+        </div>
+
+    
+        </div>
+    )
+}
+
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import Navbar from './Navbar/page'
+import Navbar from './components/navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Footer from './components/footer'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,8 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
      
-      <body className={inter.className}>{children}
-       <Navbar></Navbar>
+      <body className={inter.className}>
+        <Navbar></Navbar>
+        {children}
+        <Footer></Footer>
       </body>
     </html>
   )
