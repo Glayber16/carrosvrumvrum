b723a367f341db7da55a6fe8b0b6efa961ee0397
glayberson <glaybersonsilva@outlook.com>
2023-06-19 22:50:03 -0300

    tentando resolver

--- src/app/Funcionamento/page.js
+++ src/app/Funcionamento/page.js
@@ -2,7 +2,7 @@ import styles from "../styles/Funcionamento.module.css"
 export default function Funcionamento(){
 
     return(
-        <div className={styles.sbn}>
+        <div className={styles.funcao}>
             <h1>Sobre nossa loja</h1>
             <p>Nossa loja lhe propicia os melhores carros do mercado, temos inumeras opções de todos os tipos:</p>
             <p>Dos basicos aos ditos carros luxosos, tudo para suprir suas necessidadesm</p>
---
+++ src/app/Servicos/page.js
@@ -0,0 +1,8 @@
+import styles from "../styles/Servicos.module.css"
+export default function Servicos(){
+    return(
+        <div className={styles.serv}>
+            <h1>Serviços</h1>
+        </div>
+    )
+}
