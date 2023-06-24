a2e51323717c438944407e7d9a2f1136cc365008
glayberson <glaybersonsilva@outlook.com>
2023-06-24 14:20:26 -0300

    fotos

--- src/app/Servicos/page.js
+++ src/app/Servicos/page.js
@@ -3,10 +3,10 @@ export default function Servicos(){
     return(
         <div className={styles.serv}>
             <h1>Serviços</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                 Aperiam, obcaecati maxime. Quod accusamus, dolorem iusto ducimus 
-                 eveniet deserunt aliquam voluptatem iste quis recusandae vel commodi o
-                 fficia iure nisi porro dicta!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>  Aperiam, obcaecati maxime. Quod accusamus, dolorem iusto ducimus</p> 
+            <p>  eveniet deserunt aliquam voluptatem iste quis recusandae vel commodi o</p>
+            <p>  fficia iure nisi porro dicta!</p>
         </div>
     )
 }
